aecad769033d497c02e7ce87845830cf92fc5a9a
codeconcept <contact@codeconcept.fr>
2019-12-14 12:15:30 +0100

    Simple Redux counter + dredux devtools along with thunk

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { connect } from "react-redux";
 
-function App() {
+import "./App.css";
+
+const App = ({ dispatch, total }) => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => dispatch({ type: "ADD_NUMBER", payload: 2 })}>
+        +
+      </button>{" "}
+      {total}
     </div>
   );
-}
+};
 
-export default App;
+const mapStateToProps = state => ({ total: state.add.total });
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware, combineReducers, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+// reducer
+const add = (state = { total: 0 }, action) => {
+  if (action.type === "ADD_NUMBER") {
+    console.log("ADD_NUMBER");
+    const newState = {
+      ...state,
+      total: state.total + action.payload
+    };
+    console.log("newState", newState);
+    return newState;
+  }
+  return state;
+};
+const reducers = combineReducers({ add });
+
+const store = createStore(
+  reducers,
+  compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
